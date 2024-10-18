27b6fc68e744ace79fd816170ba2e190a981a9e4
Winner-Is-On <arthsinghal101@gmail.com>
2024-10-18 12:43:31 +0530

    css minnor changes

--- weatherapp.js
+++ weatherapp.js
@@ -13,7 +13,7 @@ const targetElement = document.getElementsByName("body");
 //initially vairables need????
 
 let oldTab = userTab;
- const API_KEY = "{CREATE YOUR OWN API KEY ON OpenWeather -->https://openweathermap.org/}";
+ const API_KEY = "46a371aa96c713cee29d72c4ac8b8a39";
 oldTab.classList.add("current-tab");
 getfromSessionStorage();
 
